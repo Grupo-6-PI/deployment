43be427c72d4fdef39f8232e89357e9cb934637d
DaviRdaSilva <davi.rsilva@sptech.school>
2025-02-24 10:09:53 -0300

    Correções de sintaxe

--- Banco/deploy_banco.sh
+++ Banco/deploy_banco.sh
@@ -2,10 +2,10 @@
 
 echo "Configuração do container MySQL para o projeto Mooca Solidária"
 
-read -p "Informe o nome do container (padrão: mooca-banco-prod): " MYSQL_CONTAINER_NAME
+read -p "Informe o nome do container (padrão: mooca-banco-prod): " CONTAINER_NAME
 CONTAINER_NAME=${CONTAINER_NAME:-mooca-banco-prod}
 
-read -p "Informe a imagem do Container (padrão: moocasolidaria/mooca-solidaria-db:latest): " MYSQL_IMAGE
+read -p "Informe a imagem do Container (padrão: moocasolidaria/mooca-solidaria-db:latest): " CONTAINER_IMAGE
 CONTAINER_IMAGE=${CONTAINER_IMAGE:-moocasolidaria/mooca-solidaria-db:latest}
 
 read -p "Informe a senha do root (padrão: urubu100): " MYSQL_ROOT_PASSWORD
@@ -44,10 +44,10 @@ if docker ps -a --format '{{.Names}}' | grep -Eq "^${CONTAINER_NAME}\$"; then
     docker rm $CONTAINER_NAME
 fi
 
-echo "Baixando a imagem do MySQL: ${CONTAINER_IMAGE}..."
+echo "Baixando a imagem do Conteiner: ${CONTAINER_IMAGE}..."
 docker pull $CONTAINER_IMAGE
 
-echo "Iniciando o container MySQL..."
+echo "Iniciando o container..."
 docker run -d \
     --name $CONTAINER_NAME \
     -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
